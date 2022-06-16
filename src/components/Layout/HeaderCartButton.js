f66192c41418d66d4f9407c88bc8dd1f121a5e2f
Goga <gogaimerlishvili1@gmail.com>
2022-06-16 23:54:43 +0400

    Modified

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 import { TiShoppingCart } from "react-icons/ti";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    console.log(item.amount);
+    return curNumber + item.amount;
+  }, 0);
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <TiShoppingCart />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
