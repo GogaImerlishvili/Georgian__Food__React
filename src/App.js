efc08c42a4c0e3b678c4d3a6f683829ce21c94f4
Goga <gogaimerlishvili1@gmail.com>
2022-07-19 21:18:44 +0400

    Added final step footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -19,6 +20,7 @@ function App() {
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
+        <Footer />
       </main>
     </CartProvider>
   );
