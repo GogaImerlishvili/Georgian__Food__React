4be00dba9b7b77d3e188c887e6c826a959c44297
Goga <gogaimerlishvili1@gmail.com>
2022-07-19 21:19:24 +0400

    Added socialIcons comp

---
+++ src/components/Layout/SocialIcons.js
@@ -0,0 +1,19 @@
+import { BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
+
+export const socialIcons = [
+  {
+    id: 1,
+    icon: <BsFacebook />,
+    url: "https://www.facebook.com/gimerlishvili5",
+  },
+  {
+    id: 2,
+    icon: <BsLinkedin />,
+    url: "https://www.linkedin.com/in/goga-imerlishvili-1984a622a/",
+  },
+  {
+    id: 3,
+    icon: <BsGithub />,
+    url: "https://github.com/GogaImerlishvili",
+  },
+];
